bc32c2ee7a6d970c6ff218e0e867933adb4c6bfd
kasunmaduraeng <kasunmaduraeng@gmail.com>
2018-07-01 17:11:39 +1200

    MCI: adding

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 #!/usr/local/bin/python3
+
+# this is  for the print some thing
+
+"""
+this is multi line string . this is not comment. this will allocate the memory
+"""
+
+
 print ("Hello, World !!")
